2bf8588d412e09e1c8d8013bcfc97f0030d5669b
yong-again <musehead91@gmail.com>
2025-09-04 01:31:02 +0000

    add anomlay_detection.py

--- configs/config.py
+++ configs/config.py
@@ -36,6 +36,19 @@ class TrainStudent:
     patch_size: int = 65
     learning_rate: float = 1e-4
     weight_decay: float = 1e-5
+    num_epochs: int = 15
+
+
+@dataclass
+class Inference:
+    root_dir: str = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    category: str = 'bottle'
+    test_size: int = 20
+    n_students: int = 3
+    patch_size: int = 65
+    image_size: int = 256
+    visualize: bool = True
+    batch_size: int = 1
 
 if __name__ == '__main__':
     config = TrainResNet()
